e3351f1978b60bfd326fbc537e63ea3e74cf1f79
moom-spotter <duddnd2574@spotter.kr>
2025-07-24 19:58:57 +0900

    Update shell scripts

--- scripts/folder_inference.sh
+++ scripts/folder_inference.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-eval "$(conda shell.bash hook)"
-conda activate FoodSAM
-
-# 사용할 GPU 디바이스 번호 지정
-export CUDA_VISIBLE_DEVICES=4
 # 이미지가 들어있는 폴더 경로와 출력 생성 경로
-python FoodSAM/semantic.py \
-  --data_root ./image_subset \
-  --output inference
+python FoodSAM/semantic.py --data_root ./image_subset --output inference
--- scripts/single_inference.sh
+++ scripts/single_inference.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-eval "$(conda shell.bash hook)"
-conda activate FoodSAM
-
-# 사용할 GPU 디바이스 번호 지정
-export CUDA_VISIBLE_DEVICES=4
-
 # 단일 테스트 이미지 경로와 결과 출력 폴더명 지정
-python FoodSAM/semantic.py \
-  --img_path dataset/FoodSeg103/Images/img_dir/test/00001977.jpg \
-  --output single_inference
+python FoodSAM/semantic.py --img_path dataset/FoodSeg103/Images/img_dir/test/00001977.jpg --output single_inference
